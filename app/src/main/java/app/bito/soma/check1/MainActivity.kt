55b71d078d3186593c1fdefc247b3ae34b3dfef4
SomaBito <kirinb8@outlook.com>
2021-06-05 15:25:55 +0900

    とちゅう。。。

--- app/src/main/java/app/bito/soma/check1/MainActivity.kt
+++ app/src/main/java/app/bito/soma/check1/MainActivity.kt
@@ -5,10 +5,19 @@ import android.os.Bundle
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    var  tapCount = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+          button.setonClickListner{
 
+            tapCount = tapCount++
+
+              val Lists: List<String> = listOf(
+
+
+          }
     }
 }
