ce86c5f3eea90c289a8f18a539464389646c35e3
SomaBito <kirinb8@outlook.com>
2021-06-05 15:01:36 +0900

    inisial commit

---
+++ app/src/main/java/app/bito/soma/check1/MainActivity.kt
@@ -0,0 +1,14 @@
+package app.bito.soma.check1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
